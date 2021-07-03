5e6adeb91717873316cbb66fbdbd47c28c00cc91
Hari Lamichhane <onwithhari@gmail.com>
2021-07-03 16:20:21 +0545

    Add method to check for reward ad availability

--- src/RNMoPubRewardedVideo.ts
+++ src/RNMoPubRewardedVideo.ts
@@ -1,7 +1,8 @@
-import { NativeEventEmitter, NativeModules } from "react-native";
+import { NativeEventEmitter, NativeModules, Platform } from "react-native";
 const { RNMoPubRewardedVideo } = NativeModules;
 
 export interface IRNMoPubRewardedVideo {
+    hasRewardedAdForAdUnit: (adUnitId: string) => Promise<boolean>;
     addEventListener: (eventType: string, listener: (args: any) => void) => void;
     initializeRewardedAd: () => void;
     loadRewardedVideoWithUnitID: (adUnitId: string) => void;
@@ -12,6 +13,16 @@ export interface IRNMoPubRewardedVideo {
 const emitter = new NativeEventEmitter(RNMoPubRewardedVideo);
 
 export default {
+    hasRewardedAdForAdUnit: async (adUnitId: string) => {
+        if (Platform.OS === 'ios') {
+            return new Promise((resolve) => {
+                RNMoPubRewardedVideo.hasAdAvailableForAdUnitID(adUnitId, (data: {'Has ad': boolean}) => {
+                    resolve(data['Has ad']);
+                });
+            });
+        }
+        return RNMoPubRewardedVideo.hasRewardedAdForAdUnit(adUnitId);
+    },
     addEventListener: (eventType, listener)  => emitter.addListener(eventType, listener),
     initializeRewardedAd: () => RNMoPubRewardedVideo.initializeRewardedAd(),
     loadRewardedVideoWithUnitID: (adUnitId) => RNMoPubRewardedVideo.loadRewardedVideoWithUnitID(adUnitId),
